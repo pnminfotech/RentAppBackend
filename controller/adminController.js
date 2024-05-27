65e3116348fa763a1777ef86b7a0bf125cca9a50
akshu12345-crypto <akshata.bhimnale@gmail.com>
2024-05-27 15:18:18 +0530

    actual status code

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,7 +1,6 @@
 // login
-//const express = require("express");
-const { log } = require("console");
 const { client } = require("../config/connection");
+
 const getLogin = async (req, res) => {
   try {
     const { emailId, password } = req.body;
@@ -10,25 +9,28 @@ const getLogin = async (req, res) => {
     const db = cl.db("StockManagementSystem");
     const collection = db.collection("Login");
 
-    const emailexists = await collection.findOne({ emailId: emailId });
-    console.log(emailId);
-    if (emailexists) {
-      const pswdexists = await collection.findOne({ password: password });
+    const user = await collection.findOne({ emailId: emailId });
 
-      if (pswdexists) {
+    if (user) {
+      const isValidPassword = await collection.findOne({
+        emailId: emailId,
+        password: password,
+      });
+
+      if (isValidPassword) {
         res.status(200).json({
-          status: true,
-          message: "Logged In Successfully ! ",
+          status: "success",
+          message: "Logged In Successfully!",
         });
       } else {
-        res.status(200).json({ status: false, message: "Wrong Password !" });
+        res.status(401).json({ status: "fail", message: "Wrong Password!" });
       }
     } else {
-      res.status(200).json({ status: false, message: "Wrong Login Id !" });
+      res.status(404).json({ status: "fail", message: "User Not Found!" });
     }
   } catch (error) {
-    console.log(error);
-    res.send(error);
+    console.error(error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
   }
 };
 
