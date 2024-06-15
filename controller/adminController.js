2c23a3cadc319ec89b2ad5a3272c4e449d47dc05
akshu12345-crypto <akshata.bhimnale@gmail.com>
2024-06-15 10:40:46 +0530

    updates

--- controller/adminController.js
+++ controller/adminController.js
@@ -192,7 +192,7 @@ const requestPasswordReset = async (req, res) => {
       subject: "Password Reset",
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        http://${req.headers.host}/reset-password/${token}\n\n
+        atock://${req.headers.host}/reset-password/${token}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
