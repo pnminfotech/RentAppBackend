5b0c625c65a5cecaaf75013d192ac3a563847678
Akshata Bhimnale <akshata.bhimnale@gmail.com>
2024-06-27 12:07:36 +0530

    updates

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 // Import route modules
 const societyRoutes = require("./societyRoutes");
 const wingRoutes = require("./wingRoutes")
-const adminRoutes = require("./adminRoutes");
 const flatRoutes = require("./flatRoutes");
 const tenantRoutes = require("./tenantRoutes");
 
@@ -14,6 +13,6 @@ router.use("/societies", societyRoutes);
 router.use("/wings", wingRoutes);
 router.use("/flats", flatRoutes);
 router.use("/tenants", tenantRoutes);
-router.use("/admin", adminRoutes);
+
 
 module.exports = router;
