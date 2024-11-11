79fe30baced13ded9445748e5d8680b10ab27f64
Pranjal Kadam <kadampranjal2002@gmail.com>
2024-11-11 15:59:53 +0530

    Initial commit for RentApp backend

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,30 +2,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("../models/User"); // Import the User model
 
-// exports.handleLogin = async (req, res) => {
-//   console.log("Login request received with body:", req.body);
-
-//   const { username, password } = req.body;
-
-//   try {
-//     const user = await User.find({ username: req.body.username });
-//     console.log("User found:", user); // Log user data
-
-//     if (user) {
-//       console.log("Comparing password:", password, "with hashed:", user.password);
-//       const isMatch = await bcrypt.compare(password, user.password);
-//       console.log('Password match result:', isMatch);
-
-//       if (isMatch) {
-//         return res.send({ success: true, message: "Login successful" });
-//       }
-//     }
-//     return res.send({ success: false, message: "Invalid username or password" });
-//   } catch (error) {
-//     console.error("Error during login:", error);
-//     res.status(500).json({ success: false, message: "Server error" });
-//   }
-// };
 exports.handleLogin = async (req, res) => {
   console.log("Login request received with body:", req.body);
 
